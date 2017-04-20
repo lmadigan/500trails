0728a223f1126aa667a338c62c3cdb7db719ea51
lmadigan <laurenmadigan51@gmail.com>
2017-04-20 16:11:23 -0700

    testing for homefeed container with store

--- __tests__/feed.test.js
+++ __tests__/feed.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import renderer from 'react-test-renderer';
 import HomeFeed from '../frontend/components/feed/home_feed.jsx';
-import HomeFeedContainer from '../frontend/components/feed/home_feed_container.jsx';
+import HomeFeedContainer from '../frontend/components/feed/home_feed_container.js';
 import configureStore from 'redux-mock-store';
+import selectTrips from '../frontend/reducers/selectors.js';
+import {Provider} from 'react-redux';
+
 
 test("When image is clicked, modal opens", () => {
   let wrapper ;
@@ -52,3 +55,45 @@ describe('Homefeed', () => {
 });
 
 //*********************************************************************
+//testing the store
+
+describe('>>HomeFeed with shallow rendering and passing {store directly}', () => {
+  const initialState = { trips: {
+    1: {
+      description:"Part of Sicily's first nature reserve",
+      id:1,
+      images: [],
+      liked: false,
+      location: "Sicily, Italy",
+      title: "Cala Capreria, Sicilia",
+      trip_user: "me",
+      user: "me"
+    },
+      2: {
+      description:"Part of Sicily's first nature reserve",
+      id: 2,
+      images: [],
+      liked: false,
+      location: "Sicily, Italy",
+      title: "Cala Capreria, Sicilia",
+      trip_user: "me",
+      user: "me"
+    }
+  }};
+  const mockStore = configureStore();
+  let store,container;
+
+  beforeEach(()=>{
+       store = mockStore(initialState);
+       container = shallow(<HomeFeedContainer store={store} /> );
+
+       it('+++ render the connected(SMART) component', () => {
+          expect(container.length).toEqual(1);
+       });
+
+       it('+++ check Prop matches with initialState', () => {
+           expect(container.prop('feedTrips')).toEqual(selectTrips(initialState.trips));
+        });
+   });
+
+});
