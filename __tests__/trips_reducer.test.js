6ff7124101215c17fbb17cd97d774c3338480df4
lmadigan <laurenmadigan51@gmail.com>
2017-04-22 23:09:09 -0700

    footer with analytics

--- __tests__/trips_reducer.test.js
+++ __tests__/trips_reducer.test.js
@@ -1,14 +1,26 @@
-import TripsReducer from '../frontend/reducers/trips_reducer.js';
-import {createTrip, deleteTrip, fetchTrips, receiveCurrentTrip, RECEIVE_TRIPS} from '../frontend/actions/trip_actions.js';
+// import {TripsReducer} from '../frontend/reducers/trips_reducer.js';
+const TripsReducer = require('../frontend/reducers/trips_reducer.js').default;
+import {createTrip, deleteTrip, fetchTrips, receiveCurrentTrip, RECEIVE_TRIPS, RECEIVE_TRIP} from '../frontend/actions/trip_actions.js';
 
 
 describe('>>>TripsReducer updates trips', () => {
-  let trips = {1:{location: "Santa Fe", title: "What a fun trip", description: "we had a blast in Santa Fe"}
+  const trips = {1:{location: "Santa Fe", title: "What a fun trip", description: "we had a blast in Santa Fe"}
   , 2: {location: "Moutain View", title: "fun fun fun", description: "I live in mountain view"}};
-  let state = {trips: {}};
+  const state = {};
   it('reducer for receiveTrips', () => {
-    state = TripsReducer({type: RECEIVE_TRIPS, trips});
-    expect(state).toEqual({trips: {1:{location: "Santa Fe", title: "What a fun trip", description: "we had a blast in Santa Fe"}
-    , 2: {location: "Moutain View", title: "fun fun fun", description: "I live in mountain view"}}});
+    let newState = TripsReducer(state, {type: RECEIVE_TRIPS, trips});
+    expect(newState).toEqual({1:{location: "Santa Fe", title: "What a fun trip", description: "we had a blast in Santa Fe"}
+    , 2: {location: "Moutain View", title: "fun fun fun", description: "I live in mountain view"}});
+  });
+});
+
+describe('>>>TripReducer updates trips', () => {
+  let state = {1:{location: "Santa Fe", title: "What a fun trip", description: "we had a blast in Santa Fe"}
+  , 2: {location: "Moutain View", title: "fun fun fun", description: "I live in mountain view"}};
+  const newTrip = {id: 3, description: "san diego", title: "san diego", location: "San Deigo"};
+  it('reducer for receiveTrip', () => {
+    let newState = TripsReducer(state, {type: RECEIVE_TRIP, trip: newTrip});
+    expect(newState).toEqual({1:{location: "Santa Fe", title: "What a fun trip", description: "we had a blast in Santa Fe"}
+    , 2: {location: "Moutain View", title: "fun fun fun", description: "I live in mountain view"}, 3: {id: 3, description: "san diego", title: "san diego", location: "San Deigo"}});
   });
 });
